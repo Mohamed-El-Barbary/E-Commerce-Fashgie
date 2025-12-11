6684b899bd69f3e366d5be9581d947f496621ce2
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2025-12-11 22:15:14 +0200

    chore(core): update Navbar to show links with routerLink and active state

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,14 +1,17 @@
-import { Component} from '@angular/core';
+import { Component, Input, input} from '@angular/core';
 import { FlowbiteService } from '../../../core/services/flowbite.service';
 import { initFlowbite } from 'flowbite';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+
+  @Input({required: true}) isLoggedIn!: boolean;
   constructor(private flowbiteService: FlowbiteService) {}
 
   ngOnInit(): void {
